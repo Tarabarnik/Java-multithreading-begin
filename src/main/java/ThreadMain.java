2b3a22ce4f5f527636c219c1cc65139708f994e0
Taranarnik <pavelkurilyuk97@gmail.com>
2019-10-30 12:05:05 +0200

    added threads racing

---
+++ src/main/java/ThreadMain.java
@@ -0,0 +1,15 @@
+import model.Counter;
+import thread.ThreadE;
+import thread.ThreadR;
+
+public class ThreadMain {
+    private static Counter counter = new Counter();
+    public static void main(String[] args) {
+
+        Thread threadR = new Thread(new ThreadR(counter));
+        ThreadE threadE = new ThreadE(counter);
+
+        threadR.start();
+        threadE.start();
+    }
+}
